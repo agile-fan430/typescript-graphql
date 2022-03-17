e26d8c80c8cea5027aac25dcbe23b6d9dfb8dc6a
ananconda <onefire430@outlook.com>
2022-03-17 15:46:40 -0400

    back to origin

--- src/schema/types/mutations.ts
+++ src/schema/types/mutations.ts
@@ -2,8 +2,6 @@ import gql from "graphql-tag";
 
 export const mutations = gql`
   type Mutation {
-
-	
     createUser(name: String!, balance: Float!): User!
     createBet(userId: Int!, betAmount: Float!, chance: Float!): Bet!
   }
